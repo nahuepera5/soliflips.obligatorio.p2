5a3a4e89f3a7924529b4083fad8dc279863ceaef
Noborita9 <joaquinbonora09@gmail.com>
2023-09-29 22:51:27 -0300

    init repo

---
+++ src/interfaz/Menu.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joaqu
+ */
+public class Menu {
+	
+}

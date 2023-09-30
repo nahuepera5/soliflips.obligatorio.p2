0e897f23485b9131276ce908fb5f882b4d96d21d
Noborita9 <joaquinbonora09@gmail.com>
2023-09-30 01:44:28 -0300

    generate random board

--- src/dominio/Tablero.java
+++ src/dominio/Tablero.java
@@ -1,18 +1,40 @@
 package dominio;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Tablero {
 
 	private Ficha[][] tablero;
 	private ArrayList<Movimiento> listaMovimientos;
+	private Ficha[][] original;
 
 	public Tablero() {
 		this.listaMovimientos = new ArrayList<Movimiento>();
 	}
 
-	public void cambiarColorEnFicha(int x, int y) {
+	public boolean retrocederMovimiento() {
+		boolean ret = false;
+
+		if (this.listaMovimientos.size() > 0) {
+			ret = true;
+			this.tablero = this.generarCopia(this.original);
+			for (int i = 0; i < this.listaMovimientos.size() - 1; i++) {
+				Movimiento m = this.listaMovimientos.get(i);
+				this.cambiarColorEnFicha(m.getX(), m.getY());
+			}
+			this.listaMovimientos.remove(this.listaMovimientos.size() - 1);
+		}
+		return ret;
+	}
+
+	public void hacerMovimiento(int x, int y) {
+		this.cambiarColorEnFicha(x, y);
 		this.listaMovimientos.add(new Movimiento(x, y));
+	}
+
+	// Todos los movimientos son por aca
+	public void cambiarColorEnFicha(int x, int y) {
 		Ficha f = this.tablero[x][y];
 		String color = f.getColorOpuesto();
 
@@ -92,15 +114,82 @@ public class Tablero {
 			{new Ficha('\\', "ROJO"), new Ficha('/', "ROJO"), new Ficha('/', "ROJO"), new Ficha('|', "AZUL"), new Ficha('/', "AZUL"), new Ficha('\\', "AZUL")},};
 
 		this.setTablero(generado);
+		this.setOriginal(this.generarCopia(generado));
 	}
 
 	public void generarAleatorio(int filas, int columnas, int nivel) {
 		// Usar un tablero creado en el momento
-		this.tablero = new Ficha[filas][columnas];
+		this.setTablero(new Ficha[filas][columnas]);
+		int xPos;
+		int yPos;
+		int fDir;
+		Ficha f = new Ficha('X', "ROJO");
+		char symbolo;
+		String color = "ROJO";
+		if ((int) (Math.random() * 2) == 1) {
+			color = "AZUL";
+		}
+
+		while (nivel > 0) {
+			xPos = (int) (Math.random() * filas);
+			yPos = (int) (Math.random() * columnas);
+			fDir = (int) (Math.random() * 4);
+			if (this.tablero[xPos][yPos] == null) {
+				if (fDir == 0) {
+					// Horizontal
+					symbolo = '-';
+				} else if (fDir == 1) {
+					// Vertical
+					symbolo = '|';
+				} else if (fDir == 2) {
+					// Diagonal Izquierda
+					symbolo = '\\';
+				} else {
+					// Digaonal Derecha
+					symbolo = '/';
+				}
+				f = new Ficha(symbolo, color);
+				this.tablero[xPos][yPos] = f;
+				nivel--;
+			}
+		}
+		color = f.getColorOpuesto();
+		for (int i = 0; i < this.tablero.length; i++) {
+			for (int j = 0; j < this.tablero[0].length; j++) {
+				if (this.tablero[i][j] == null) {
+					fDir = (int) (Math.random() * 4);
+					if (fDir == 0) {
+						// Horizontal
+						symbolo = '-';
+					} else if (fDir == 1) {
+						// Vertical
+						symbolo = '|';
+					} else if (fDir == 2) {
+						// Diagonal Izquierda
+						symbolo = '\\';
+					} else {
+						// Digaonal Derecha
+						symbolo = '/';
+					}
+					this.tablero[i][j] = new Ficha(symbolo, color);
+				}
+			}
+		}
+		this.setOriginal(this.generarCopia(this.tablero));
 	}
 
 	public void generarPorLectura() {
 		// Usar un tablero especificado en el archivo "datos.txt"
+	}
+
+	private Ficha[][] generarCopia(Ficha[][] tablero) {
+		Ficha[][] copia = new Ficha[tablero.length][tablero[0].length];
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[0].length; j++) {
+				copia[i][j] = new Ficha(tablero[i][j].getSymbolo(), tablero[i][j].getColor());
+			}
+		}
+		return copia;
 	}
 
 	public Ficha[][] getTablero() {
@@ -117,6 +206,14 @@ public class Tablero {
 
 	private void setListaMovimientos(ArrayList<Movimiento> listaMovimientos) {
 		this.listaMovimientos = listaMovimientos;
+	}
+
+	private void setOriginal(Ficha[][] original) {
+		this.original = original;
+	}
+
+	private Ficha[][] getOriginal() {
+		return this.original;
 	}
 
 }
--- src/interfaz/Menu.java
+++ src/interfaz/Menu.java
@@ -4,14 +4,8 @@ import dominio.Ficha;
 import dominio.Tablero;
 import java.util.Arrays;
 
-
 public class Menu {
+
 	public static void main(String[] args) {
-		Tablero tab = new Tablero();
-		tab.generarPredeterminado();
-		tab.cambiarColorEnFicha(0, 4);
-		for (Ficha[] row: tab.getTablero()){
-			System.out.println(Arrays.toString(row));
-		}
 	}
 }
